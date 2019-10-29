3d50ce87e353885953e493fccba685c9f6d17658
Jesse Moore <jesse.moore@adtran.com>
2019-10-29 18:09:07 -0500

    finished first pass in backpropigation

--- ann.py
+++ ann.py
@@ -13,16 +13,20 @@ class Neuron:
     def __init__(self):
         self.activation_value = None
         self.net_input = None
+        self.dJ_wrt_Z = None
         self.weights_in = []
+        self.dJs_wrt_Wi = []
         self.bias_in = None
-        self.deriv_cost_wrt_net_input = None
+        self.dJ_wrt_B = None
 
     def __str__(self):
         return "\n".join([
             "activation_value: {}".format(self.activation_value),
             "net_input: {}".format(self.net_input),
             "weights_in: {}".format(self.weights_in),
+            "dJs_wrt_Wi: {}".format(self.dJs_wrt_Wi), 
             "bias_in: {}".format(self.bias_in),
+            "dJ_wrt_B: {}".format(self.dJ_wrt_B), 
         ])
 
 
@@ -34,14 +38,14 @@ class BinaryNeuron(Neuron):
         return 1/(1+exp(-x))
 
     def binary_sigmoid_derivative(self, x):
-        return binary_sigmoid(x)*(1 - binary_sigmoid(x))
+        return self.binary_sigmoid(x)*(1 - self.binary_sigmoid(x))
 
     def calculate_activation_and_store_input(self, net_input):
         self.net_input = net_input
         self.activation_value = self.binary_sigmoid(net_input)
 
     def calculate_deriv_activation_wrt_net_input(self):
-        self.deriv_activation_wrt_net_input = self.binary_sigmoid_derivative(net_input)
+        return self.binary_sigmoid_derivative(self.net_input)
 
 class BipolarNeuron(Neuron):
     def __init__(self):
@@ -51,14 +55,14 @@ class BipolarNeuron(Neuron):
         return (exp(x)-exp(-x))/(exp(x)+exp(-x))
 
     def bipolar_sigmoid_derivative(self, x):
-        return 1 - bipolar_sigmoid(x)**2
+        return 1 - self.bipolar_sigmoid(x)**2
 
     def calculate_activation_and_store_input(self, net_input):
         self.net_input = net_input
         self.activation_value = self.bipolar_sigmoid(net_input)
 
     def calculate_deriv_activation_wrt_net_input(self):
-        self.deriv_activation_wrt_net_input = self.bipolar_sigmoid_derivative(net_input)
+        return self.bipolar_sigmoid_derivative(self.net_input)
 
 
 NEURON_MAP = {'BinaryNeuron': BinaryNeuron, 'BipolarNeuron': BipolarNeuron}
@@ -134,15 +138,20 @@ class NeuralNetwork:
                 assert elem['neuron_type'], "Must define a neuron type for each hidden layer."
                 assert elem['num_units'], "Must define a number of units for each hidden layer."
 
+    def _has_learning_rate(self, config):
+        assert config['learning_rate'], "Must define a learning rate to use in gradient descent."
+
     def _validate_and_store_config(self, config):
         self._has_valid_input_layer_params(config)
         self._has_valid_output_layer_params(config)
         self._input_and_target_lengths_match(config)
         self._has_valid_hidden_layer_params(config)
+        self._has_learning_rate(config)
         self.inputs = config['input_layer']['input_vectors']
         self.targets = config['output_layer']['target_vectors']
         self.output_neuron_type = config['output_layer']['neuron_type']
         self.hidden_layer_data = config['hidden_layers']
+        self.learning_rate = config['learning_rate']
 
     def _construct_layers(self):
         self.layers.append(InputLayer(self.inputs[0]))
@@ -205,11 +214,13 @@ class NeuralNetwork:
         return (2/n) * (Yj - Tj)
 
     def back_propigate(self):
+        #layer counter
+        l = len(self.layers) - 1
         #Start with the output layer
         #first we are calculating gradient of loss wrt net input for each node in activation layer
         #grad_J_wrt_Zj[j]: Gradient of J wrt net input for jth node
         grad_J_wrt_Zj = []
-        for j, neuron in enumerate(self.layers[-1].neurons):
+        for j, neuron in enumerate(self.layers[l].neurons):
             #Gradient of error wrt net input for each neuron in output
             #calculated by chain rule GRAD(J, Zj) = J wrt Yj x Yj wrt Zj
             # dJ/dYj is derivative of mse wrt Yj or derivative of loss wrt output
@@ -222,31 +233,73 @@ class NeuralNetwork:
             # since this is dependent on the type of neuron being used
             # we delegate it to the neuron. This is derivative of activation
             # function wrt to the net input to the activation function
-            neuron.calculate_deriv_activation_wrt_net_input()
-            grad_J_wrt_Zj.append(J_wrt_Yj * neuron.deriv_activation_wrt_net_input)
+            neuron.dJ_wrt_Z = J_wrt_Yj * neuron.calculate_deriv_activation_wrt_net_input()
 
         #next, we calculate the gradients for loss with respect to the weights and bias
         #Gradient of error wrt Wij calculates the influence of the weight between neuron i in layer l-1
         #to the jth node in current layer l. Gradient of error wrt Bj is influence of bias on neuron j
         #in layer l
-        for j, neuron in enumerate(self.layers[-1].neurons):
-            for i, previous_neuron enumerate(self.layers[-2].neurons):
-                pass
+        for i, neuron in enumerate(self.layers[l].neurons):
+            for j, neuron_in_previous_layer in enumerate(self.layers[l-1].neurons):
+                #Grad J wrt Wij = Grad_J_wrt_Zj[j] * dZj/dWij
+                #dZj/dWij = previous_neuron.activation_value since all other weights are ignored
+                dZj_wrt_Wij = neuron_in_previous_layer.activation_value
+                dJ_wrt_Wij = neuron.dJ_wrt_Z * dZj_wrt_Wij
+                neuron.dJs_wrt_Wi.append(dJ_wrt_Wij)
+            #Grad J wrt Bj = Grad_J_wrt_Zj[j] * dZj/DBj
+            #dZj/dBj = 1 since the bias has no associated value
+            dZj_wrt_Bj = 1
+            dJ_wrt_Bj = neuron.dJ_wrt_Z * dZj_wrt_Bj
+            neuron.dJ_wrt_B = dJ_wrt_Bj
 
+        #We can then calculate the gradient of cost with respect to net input for the nodes
+        #in the previous layer with the gradients we've already calculated, we calculate from
+        #the output layer to the input layer.
+        l = l - 1
+        #while we are not on the input layer, step backwards caclculating the weight and 
+        #bias adjustments
+        while l >= 1:
+            print('l: {}'.format(l))
+            for j, neuron in enumerate(self.layers[l].neurons):
+                sum_of_dJdZi_and_Wij = 0
+                for i, neuron_in_next_layer in enumerate(self.layers[l+1].neurons):
+                    sum_of_dJdZi_and_Wij = sum_of_dJdZi_and_Wij + (neuron_in_next_layer.dJ_wrt_Z * neuron_in_next_layer.weights_in[j])
+                neuron.dJ_wrt_Z = sum_of_dJdZi_and_Wij * neuron.calculate_deriv_activation_wrt_net_input()
+                for k, neuron_in_previous_layer in enumerate(self.layers[l-1].neurons):
+                    dZj_wrt_Wij = neuron_in_previous_layer.activation_value
+                    dJ_wrt_Wij = neuron.dJ_wrt_Z * dZj_wrt_Wij
+                    #print("k: {}, j: {}".format(k, j))
+                    #print("dZj_wrt_Wij: {}, neuron.dJ_wrt_Z: {}, dJ_wrt_Wij: {}".format(dZj_wrt_Wij, neuron.dJ_wrt_Z,  dJ_wrt_Wij))
+                    neuron.dJs_wrt_Wi.append(dJ_wrt_Wij)
+                dZj_wrt_Bj = 1
+                dJ_wrt_Bj = neuron.dJ_wrt_Z * dZj_wrt_Bj
+                neuron.dJ_wrt_B = dJ_wrt_Bj
+            l = l - 1
+
+        #finally, update all weights and biases using gradient descent
+        for l, layer in enumerate(self.layers):
+            #layer 0 doesn't have any preceeding weights and biases
+            if l == 0:
+                continue
+            for neuron in layer.neurons:
+                for i, weight in enumerate(neuron.weights_in):
+                    weight = weight - (self.learning_rate * neuron.dJs_wrt_Wi[i])
+                neuron.bias_in = neuron.bias_in - (self.learning_rate * neuron.dJ_wrt_B)
 
     def train(self):
-
         #feed forward input vector
         self.feed_forward()
 
         #calculate loss using MSE
         J = self.mse()
 
-        print(J)
+        #Propigate the loss back through the network
+        self.back_propigate()
 
         #rotate input and target for next training call
         self.inputs = self.inputs[1:] + self.inputs[:1]
         self.targets = self.targets[1:] + self.targets[:1]
+        return J
 
 
 ann = NeuralNetwork({
@@ -261,7 +314,9 @@ ann = NeuralNetwork({
         'target_vectors': BINARY_T,
         'neuron_type': 'BinaryNeuron'
     },
+    'learning_rate': .1,
 })
+
 print(ann)
 ann.train()
 print(ann)
